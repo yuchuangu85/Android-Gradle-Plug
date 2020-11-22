30d875570400e76ff665ffa77b4e3a29d9970b31
yuchuangu85 <yuchuangu85@gmail.com>
2020-11-22 21:55:07 +0800

    添加插件实例

---
+++ customplugin/src/main/java/com/codemx/plugin/CustomPlugin.java
@@ -0,0 +1,11 @@
+package com.codemx.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class CustomPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+
+    }
+}
